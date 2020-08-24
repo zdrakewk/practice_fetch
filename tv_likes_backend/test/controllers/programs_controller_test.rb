deebff7afadfc2a627e6da3124c07acfb9f8f2fe
github username <github email address>
2020-08-23 20:19:25 -0500

    remove unnecessary files/folders

--- tv_likes_backend/test/controllers/programs_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ProgramsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get programs_index_url
-    assert_response :success
-  end
-
-end
